ccdad099823ada9eaf9b660ff863995dfb42d48f
AviSoomirtee <avisoomirtee.as@gmail.com>
2017-10-30 00:02:58 +0400

    updated
    30/10/17

---
+++ gps_read_data.py
@@ -0,0 +1,22 @@
+import serial
+
+gps = serial.Serial("/dev/ttyACM0", baudrate = 9600)
+
+while True:
+        line = gps.readline()
+        data = line.split(",")
+        if data[0] == "$GPRMC":
+                if data[2] =="A":
+                        raw_lat = data[3]
+                        raw_long = data[5]
+                        f_lat = float(raw_lat[:2])
+                        s_lat = float(raw_lat[2:])
+                        lat = (( (s_lat/60)+f_lat )*(-1))
+                        print round(lat,5)
+                        f_long = float(raw_long[:3])
+                        s_long = float(raw_long[3:])
+                        long = ( (s_long/60) + f_long)
+                        print round(long,5)
+                #       print f_long
+                #       print "Laltitude: %s" % (data[3])
+                #       print "longitude: %s" % (data[5]) 
---
+++ gps_to_db.py
@@ -0,0 +1,31 @@
+#!usr/bin/python
+
+import MySQLdb
+import serial
+
+db = MySQLdb.connect("localhost","root","adminyolo","carData")
+curs = db.cursor()
+gps = serial.Serial("/dev/ttyACM0", baudrate =9600)
+while True:
+        line = gps.readline()
+        data = line.split(",")
+        if data[0] == "$GPRMC":
+                if data[2] =="A":
+                        raw_lat = data[3]
+                        raw_long = data[5]
+                        f_lat = float(raw_lat[:2])
+                        s_lat = float(raw_lat[2:])
+                        lat = round((( (s_lat/60)+f_lat )*(-1)),6)
+                       # print round(lat,5)
+                        f_long = float(raw_long[:3])
+                        s_long = float(raw_long[3:])
+                        long = round(( (s_long/60) + f_long),6)
+                       # print round(long,5)
+                        try:
+                          curs.execute ("update data set latt=%s where id='0'" %lat)
+                          curs.execute ("update data set longg=%s where id='0'" %long)
+                          db.commit()
+                          print "Data committed"
+                        except:
+                            print " Error: the database is being rolled back"
+                            db.rollback()
